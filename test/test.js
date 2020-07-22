31fd6f3850dd7334955732abc0234464eb112243
Eric Dobbs <eric@dobbse.net>
2020-07-22 16:50:04 -0600

    sketch test cases for adding STATIC keyword to graphviz markup
    The interaction design here aims to remain close to the editing
    experience of other wiki plugins. If we can make this work, we can
    empower authors without change to the existing text editor.
    
    It may be worth challenging this design decision. Current practice
    steers authors towards innovating via uppercase words in the plugin
    markup language. Should we encourage plugin authors to experiment more
    with custom editors instead?
    
    For this experiment, we choose current practice.

--- test/test.js
+++ test/test.js
@@ -12,6 +12,24 @@
         return expect(result).to.be('hello <i>world</i>');
       });
     });
+
+    describe('authors can convert algorithmic diagrams into static diagrams', () => {
+      context('when text omits both DOT and STATIC', () => {
+        // static diagram
+      }) ;
+      context('when text contains DOT and omits STATIC', () => {
+        // algorithmic diagram
+      }) ;
+      context('when text contains both DOT and STATIC', () => {
+        // hopefully we can prevent this case
+        // or just clean it up by removing the STATIC block
+      }) ;
+      context('when text contains omits DOT and contains STATIC', () => {
+        // hopefully we can prevent this case or just clean it up by
+        // removing the STATIC keyword and preserving the remaining
+        // content
+      }) ;
+    });
   });
 
 }).call(this);
